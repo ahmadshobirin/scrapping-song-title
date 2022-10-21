bb2604690985141cd8042d2b666058539643959b
ahmadshobirin <ahmadshobirin.dev@gmail.com>
2022-10-21 19:57:25 +0700

    save fo csv file

---
+++ README.md
@@ -0,0 +1,8 @@
+## Scrapping Songs From Website
+
+Basically just retrieve the song title information in a website. However, websites always have a different HTML DOM structure from the others
+
+### How To Use
+* run try.py `python3 try.py`
+* wait a moment, the results will appear in the file song_scrapper.csv
+* if u wanna make sure just deleted all hole data from song_scrapper.csv and run again
--- try.py
+++ try.py
@@ -1,6 +1,9 @@
 import requests
 from pprint import pprint
 from bs4 import BeautifulSoup
+import csv
+from pathlib import Path
+
 
 r = requests.get("https://www.gramedia.com/best-seller/lagu-pop-indonesia-terpopuler/")
   
@@ -13,11 +16,15 @@ i = 1
 for row in listDivs.findAll('h3'):
     song = {}
     song['Nomor'] = i
-
     songTitle = row.text
     songTitleWithoutNumber = ''.join([i for i in songTitle if not i.isdigit()])
     song['Title'] = songTitleWithoutNumber.strip('. \xa0.')
+    print(song, "\n")
     musics.append(song)
     i+=1
 
-pprint(musics)
+with open("song_scrapper.csv", 'w', newline='') as f:
+    w = csv.DictWriter(f,['Nomor','Title'])
+    w.writeheader()
+    for music in musics:
+        w.writerow(music)
